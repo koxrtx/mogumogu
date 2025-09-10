e8ed98455e53ecb09df23c3a820db2975dcda9d7
koxrtx <koxrtx2003@yahoo.co.jp>
2025-09-10 23:13:47 +0900

    feat:問い合わせモデルenum追加_enumのi18n化/docs:ER図修正_問い合わせテーブルにStatuseカラム追加

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -3,4 +3,6 @@ class Inquiry < ApplicationRecord
   validates :name, presence: true
   validates :mail, presence: true
   validates :inquiry_comment, presence: true
+
+  enum status: { pending: 0, in_progress: 1, completed: 2 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_10_132923) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_10_140630) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_10_132923) do
     t.string "inquiry_comment", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0, null: false
   end
 
   create_table "spot_facilities", force: :cascade do |t|
