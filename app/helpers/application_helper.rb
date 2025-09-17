c471c3f19dfbb5900702041ebe8c0857ed874634
koxrtx <koxrtx2003@yahoo.co.jp>
2025-09-17 12:09:17 +0900

    feat:gem devise追加・User登録画面フロント修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def input_classes
+    "w-full px-3 py-2 border border-blue-400 rounded bg-sky-50 focus:outline-none focus:ring focus:border-blue-300 mb-4"
+  end
+
+  def button_classes
+    "w-full bg-sky-500 text-white py-2 rounded hover:bg-sky-600 transition"
+  end
 end
