bbbc0c8fab63ccf69c96c8f551c414b58ec185a9
koxrtx <koxrtx2003@yahoo.co.jp>
2025-09-18 10:54:31 +0900

    regactor:各ページの共通レイアウトをヘルパーメゾット化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  # 各ページの最初の設定
+  def page_classes
+    "min-h-screen bg-sky-200 flex flex-col items-center justify-start"
+  end
+  # タイトル
+  def page_title
+    "text-3xl font-bold text-gray-700 text-center mt-8 mb-8"
+  end
+
   # ユーザー登録で使ったメゾット
   def input_classes
     "w-96 h-12 px-3 py-2 border border-blue-400 rounded bg-sky-50 focus:outline-none focus:ring focus:border-blue-300 mb-4"
