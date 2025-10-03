28130a95ab8224b6461752ca8672d8f938b3975d
koxrtx <koxrtx2003@yahoo.co.jp>
2025-10-03 13:19:00 +0900

    feat:ホーム画面のボタン・ヘッダーのリンクどちらからでも現在地取得のイベントが実行されるように設計

--- app/javascript/location.js
+++ app/javascript/location.js
@@ -1,38 +1,35 @@
-// ホームページの「現在地から探す」ボタン処理
-// ボタン押したら画面遷移して現在地取得する
-console.log("location.js読み込み")
-
-document.addEventListener('turbo:load', function(){
-console.log("発火2");
-
+// ホーム画面ボタンとヘッダーのリンク両方に
+// moveToCurrentLpcationを実行するイベントをつける
+document.addEventListener('turbo:load', function() {
   const locationButton = document.getElementById("current-location-btn");
-  console.log("locationButton:", locationButton);
-    if (!locationButton) return; // 要素がなければ処理しない
+  const locationLink = document.getElementById("current-location-link");
 
-  locationButton.addEventListener("click", () => {
-    console.log("ボタンクリック")
-    // ブラウザがユーザーの現在地（緯度・経度）を取得するためのAPI
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-
-          // 画面遷移処理
-          window.location.href = `/maps/search?lat=${pos.lat}&lng=${pos.lng}`;
-        },
-
-        // エラー処理
-        () => {
-          alert("位置情報の取得に失敗しました。ブラウザの設定を確認してください。");
-        },
-      );
-    } else {
-      // ブラウザが位置情報をサポートしていない場合
-      alert("お使いのブラウザは位置情報をサポートしていません")
-    }
-  });
+  if (locationButton) locationButton.addEventListener("click", moveToCurrentLocation);
+  if (locationLink) locationLink.addEventListener("click", moveToCurrentLocation);
 });
+
+// 現在地取得
+function moveToCurrentLocation(event) {
+  if (event) event.preventDefault(); // リンクの場合はページ遷移を止める
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        // 画面遷移
+        window.location.href = `/maps/search?lat=${pos.lat}&lng=${pos.lng}`;
+      },
+      // エラー処理
+      () => {
+        alert("位置情報の取得に失敗しました。ブラウザの設定を確認してください。");
+      }
+    );
+  } else {
+    // ブラウザが位置情報をサポートしていない場合
+    alert("お使いのブラウザは位置情報をサポートしていません");
+  }
+}
 
