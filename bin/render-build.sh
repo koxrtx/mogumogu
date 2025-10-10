24ed13db74742b40212df2a749c60084b957b62d
koxrtx <koxrtx2003@yahoo.co.jp>
2025-10-10 14:44:57 +0900

    修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,4 +4,5 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
+bundle exec rails db:migrate:queue
 bundle exec rails solid_queue:install
