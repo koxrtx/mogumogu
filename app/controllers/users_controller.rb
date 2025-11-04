0bb6f01f0079590d26de5835bc31ccf1ff47dbb6
koxrtx <koxrtx2003@yahoo.co.jp>
2025-11-04 10:17:31 +0900

    LINEログインのマイページ設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+  
   # マイページ表示
   def mypage
     @user = current_user
@@ -11,18 +14,34 @@ class UsersController < ApplicationController
 
   # 更新
   def update_mypage
-    @user = current_user
-    if @user.update(user_paramus)
+
+    # LINEユーザーか
+    if @user.line_user?
+      permitted_params = user_params_for_line_user
+    else
+      permitted_params = user_params_for_normal_user
+    end
+
+    if @user.update(permitted_params)
       redirect_to mypage_path, notice: "プロフィールを更新しました"
     else
-      render :edit_mypage
+      render :edit, alert: 'プロフィールの更新に失敗しました'
     end
   end
 
   private
 
-  # パスワード・メアドはDevise管理のため不要
-  def user_paramus
+  def set_user
+    @user = current_user
+  end
+
+  def user_params_for_line_user
+    params.require(:user).permit(:name)
+  end
+
+    # パスワード・メアドはDevise管理のため不要
+  def user_params_for_normal_user
+    # 通常ユーザーでも、メール・パスワードは別ページで変更
     params.require(:user).permit(:name)
   end
 end
