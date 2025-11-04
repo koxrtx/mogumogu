4e0417de6338001d276a428d1b2a7b1705d565cb
koxrtx <koxrtx2003@yahoo.co.jp>
2025-11-04 23:44:51 +0900

    feat:フロント修正

---
+++ app/javascript/line_login.js
@@ -0,0 +1,29 @@
+document.addEventListener("turbo:load", () => {
+  const button = document.getElementById('line-login-btn');
+  const icon = document.getElementById('line-icon-official');
+  if (!button || !icon) return;
+
+  const baseSrc = button.dataset.baseImage;
+  const hoverSrc = button.dataset.hoverImage;  
+  const pressSrc = button.dataset.pressImage;
+
+  // ホバー時
+  button.addEventListener('mouseenter', () => {
+    icon.src = hoverSrc;
+  });
+
+  // プレス時
+  button.addEventListener('mousedown', () => {
+    icon.src = pressSrc;
+  });
+
+  // マウスアップ時
+  button.addEventListener('mouseup', () => {
+    icon.src = hoverSrc;
+  });
+
+  // マウスが離れた時
+  button.addEventListener('mouseleave', () => {
+    icon.src = baseSrc;
+  });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"M PLUS Rounded 1c"', 'sans-serif'], // デフォルトsansに置き換え
+        rounded: ['"M PLUS Rounded 1c"', 'sans-serif'],
+      },
+    },
+  },
   content: [
     './app/views/**/*.html.erb',
+    './app/views/**/*.slim',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
-  plugins: ["daisyui"]
+  plugins: ["daisyui"],
 }
