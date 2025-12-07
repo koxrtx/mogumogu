2b81824bab31d932db06476484052b6e39866940
koxrtx <koxrtx2003@yahoo.co.jp>
2025-12-08 00:12:22 +0900

    マイグレーション修正

--- db/migrate/20251207124734_change_request_type_to_integer.rb
+++ db/migrate/20251207124734_change_request_type_to_integer.rb
@@ -1,19 +1,27 @@
 class ChangeRequestTypeToInteger < ActiveRecord::Migration[7.0]
   def up
     # 本番では値が空かもしれないので安全に
-    execute <<-SQL
-      UPDATE spot_image_update_requests
-      SET request_type = '0'
-      WHERE request_type = 'add';
-    SQL rescue nil
+    begin
+      execute <<-SQL
+        UPDATE spot_image_update_requests
+        SET request_type = '0'
+        WHERE request_type = 'add';
+      SQL
+    rescue
+      nil
+    end
 
-    execute <<-SQL
-      UPDATE spot_image_update_requests
-      SET request_type = '1'
-      WHERE request_type = 'remove';
-    SQL rescue nil
+    begin
+      execute <<-SQL
+        UPDATE spot_image_update_requests
+        SET request_type = '1'
+        WHERE request_type = 'remove';
+      SQL
+    rescue
+      nil
+    end
 
-    # 文字列 '0', '1' を整数に変換
+    # 文字列 '0' / '1' を整数にキャスト
     change_column :spot_image_update_requests, :request_type, :integer, using: 'request_type::integer'
   end
 
