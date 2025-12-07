e94437d89711095700e25be98d0acd962bff4178
koxrtx <koxrtx2003@yahoo.co.jp>
2025-12-07 23:36:34 +0900

    refact/マイグレーションし直し

--- db/migrate/20251207124734_change_request_type_to_integer.rb
+++ db/migrate/20251207124734_change_request_type_to_integer.rb
@@ -1,11 +1,14 @@
 class ChangeRequestTypeToInteger < ActiveRecord::Migration[7.0]
   def up
-    # 既存のデータを一旦削除するか、変換する
-    # 開発環境で問題なければ削除
-    SpotImageUpdateRequest.delete_all
-    
-    # カラムの型を変更
-    change_column :spot_image_update_requests, :request_type, :integer, using: 'request_type::integer'
+    execute <<-SQL
+      UPDATE spot_image_update_requests
+      SET request_type = CASE
+        WHEN request_type = 'add' THEN 0
+        WHEN request_type = 'remove' THEN 1
+      END
+    SQL
+
+    change_column :spot_image_update_requests, :request_type, 'integer USING request_type::integer'
   end
 
   def down
