6cf0111e1659253d9ff31549f78e2562a3dbddaf
koxrtx <koxrtx2003@yahoo.co.jp>
2025-12-08 00:01:36 +0900

    refact/マイグレーション再度やり直し

--- db/migrate/20251207124734_change_request_type_to_integer.rb
+++ db/migrate/20251207124734_change_request_type_to_integer.rb
@@ -1,19 +1,10 @@
 class ChangeRequestTypeToInteger < ActiveRecord::Migration[7.0]
   def up
-    # 1. 文字列を整数に置き換える
-    execute <<-SQL
-      UPDATE spot_image_update_requests
-      SET request_type = 0
-      WHERE request_type = 'add';
-    SQL
-
-    execute <<-SQL
-      UPDATE spot_image_update_requests
-      SET request_type = 1
-      WHERE request_type = 'remove';
-    SQL
-
-    # 2. カラム型を整数に変更
+    # 既存のデータを一旦削除するか、変換する
+    # 開発環境で問題なければ削除
+    SpotImageUpdateRequest.delete_all
+    
+    # カラムの型を変更
     change_column :spot_image_update_requests, :request_type, :integer, using: 'request_type::integer'
   end
 
