2e6ba91228acb43892cef116dc1e156b4983695f
koxrtx <koxrtx2003@yahoo.co.jp>
2025-12-15 13:35:07 +0900

    feat/spotsコントローラーのrspec追加

--- spec/requests/spots_spec.rb
+++ spec/requests/spots_spec.rb
@@ -1,79 +1,79 @@
 require 'rails_helper'
 
-RSpec.describe "Spots", type: :request do
+RSpec.describe SpotsController, type: :request do
   let(:user) { create(:user) }
-  let(:spot) { create(:spot) }
+  let(:spot) { create(:spot, user: user) }
 
-  # Devise（デバイズ）ログイン
-  before do
-    sign_in user
-  end
+  describe 'GET /spots/new' do
+    context 'ログインしている場合' do
+      before do
+        login_as(user, scope: :user)
+      end
 
-  # GET /spots/new
-  describe "GET /new" do
-    it "new テンプレートを表示する" do
-      get new_spot_path
-      expect(response).to have_http_status(:ok)
-      expect(response.body).to include("新規スポット") # 画面の文字に合わせて調整
-    end
-  end
-
-  # GET /spots/:id
-  describe "GET /spots/:id" do
-    it "スポットの詳細ページを表示する" do
-      get spot_path(spot)
-      expect(response).to have_http_status(:ok)
-      expect(response.body).to include("店A")
-    end
-  end
-
-
-  # POST /spots
-  describe "POST /spots" do
-    context "有効なデータのとき" do
-      let(:valid_params) {
-        {
-          spot: {
-            name: "テスト店",
-            address: "東京都新宿区",
-            tel: "09012345678",
-            opening_hours: "10:00〜20:00",
-            latitude: 35.0,
-            longitude: 139.0,
-            business_status: "open",
-            child_chair: true,
-            tatami_seat: false,
-            child_tableware: true,
-            bring_baby_food: true,
-            stroller_ok: true,
-            child_menu: false,
-            parking: true,
-            other_facility: "特になし"
-          }
-        }
-      }
-
-      it "スポットが保存される" do
-        expect {
-          post spots_path, params: valid_params
-        }.to change(Spot, :count).by(1)
-
-        expect(response).to have_http_status(:found)
-        expect(response).to redirect_to(Spot.last)
+      it '新規投稿ページが表示される' do
+        get new_spot_path
+        expect(response).to have_http_status(:success)
       end
     end
+  end
 
-    context "無効なデータのとき" do
-      let(:invalid_params) {
-        { spot: { name: "", address: "" } }
-      }
+  describe 'GET /spots/:id' do
+    it 'スポット詳細が表示される' do
+      get spot_path(spot)
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-      it "保存されず new を再表示する" do
-        expect {
+  describe 'POST /spots' do
+    context 'ログインしている場合' do
+      before do
+        login_as(user, scope: :user)
+      end
+
+      context 'パラメーターが正しい時' do
+        let(:valid_params) do
+          {
+            spot: {
+              name: 'テスト店舗',
+              address: '東京都',
+              tel: '0312345678',
+              opening_hours: '10:00-18:00',
+              latitude: 35.0,
+              longitude: 139.0
+            }
+          }
+        end
+
+        it 'スポットが作成される' do
+          expect {
+            post spots_path, params: valid_params
+          }.to change(Spot, :count).by(1)
+        end
+
+        it '詳細ページにリダイレクトされる' do
+          post spots_path, params: valid_params
+          expect(response).to redirect_to(Spot.last)
+        end
+
+        it 'thanks フラグが立つ' do
+          post spots_path, params: valid_params
+          expect(flash[:thanks]).to be_truthy
+        end
+      end
+
+      context 'パラメーターが不正な時' do
+        let(:invalid_params) do
+          {
+            spot: {
+              name: ''
+            }
+          }
+        end
+
+        it '新規投稿ページが再表示される' do
           post spots_path, params: invalid_params
-        }.not_to change(Spot, :count)
-
-        expect(response).to have_http_status(:unprocessable_content)
+          expect(response).to have_http_status(:unprocessable_content)
+        end
       end
     end
   end
